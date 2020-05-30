a0692879d6900d71077d7ec32c6712b6975a6fa6
mhlee304 <mhlee304@gmail.com>
2020-05-30 12:15:02 -0500

    Added create table, add username, and fetch_response

--- src/rest_test_app.erl
+++ src/rest_test_app.erl
@@ -6,13 +6,17 @@
 -module(rest_test_app).
 -behaviour(application).
 
+-record(project, {call,
+    response}).
+
+
 %% API.
--export([start/2]).
+-export([start/2, install/0, add_username/2, fetch_response/1] ).
 -export([stop/1]).
 
-%% API.
 
 start(_Type, _Args) ->
+
     Dispatch = cowboy_router:compile([
         {'_', [
             {"/", toppage_h, []}
@@ -22,6 +26,36 @@ start(_Type, _Args) ->
         env => #{dispatch => Dispatch}
     }),
     rest_test_sup:start_link().
+
+%%creates table called project
+install() ->
+    mnesia:create_schema([node()]),
+    mnesia:start(),
+
+    mnesia:create_table(project,
+        [{attributes, record_info(fields, project)}]).
+
+%%Adds username with response into database
+add_username(Call, Response) ->
+    F = fun() ->
+        mnesia:write(#project{call=Call,
+            response=Response
+            })
+        end,
+    mnesia:activity(transaction, F).
+
+%%Finds reponse associated with call
+fetch_response(Call) ->
+    Pattern = #project{_ = '_',
+        call = Call},
+    F = fun() ->
+        Res = mnesia:match_object(Pattern),
+        [{Call,R} ||
+            #project{call=Call,
+                response=R
+                } <- Res]
+        end,
+    mnesia:activity(transaction, F).
 
 stop(_State) ->
     ok = cowboy:stop_listener(http).
